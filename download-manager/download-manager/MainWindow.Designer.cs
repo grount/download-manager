6ea100df7c126e962744f67924cddfe20023bbf0
Daniel Gront <grount1@gmail.com>
2017-04-05 18:41:52 +0300

    Ongoing modifications in http client information.

--- download-manager/download-manager/MainWindow.Designer.cs
+++ download-manager/download-manager/MainWindow.Designer.cs
@@ -28,20 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.downloadSaveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.SuspendLayout();
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(13, 13);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(48, 13);
+            this.urlLabel.TabIndex = 0;
+            this.urlLabel.Text = "File URL";
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(13, 30);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(313, 20);
+            this.urlTextBox.TabIndex = 1;
+            this.urlTextBox.TextChanged += new System.EventHandler(this.urlTextBox_TextChanged);
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(332, 30);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(75, 23);
+            this.downloadButton.TabIndex = 2;
+            this.downloadButton.Text = "Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(663, 255);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.urlLabel);
             this.Name = "MainWindow";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.SaveFileDialog downloadSaveFileDialog;
     }
 }
 
--- download-manager/download-manager/MainWindow.cs
+++ download-manager/download-manager/MainWindow.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,68 @@ namespace download_manager
 {
     public partial class MainWindow : Form
     {
+        string m_downloadUrl;
+        string m_selectedPath;
+        bool m_isSaveSelected;
+        bool m_isUrlSelected;
+
         public MainWindow()
         {
             InitializeComponent();
+            m_isSaveSelected = false;
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            await httpClientGetAsync();
+            saveTheFileDialog();
+
+            if (m_isSaveSelected == false)
+            {
+                invalidPath();
+            }
+        }
+
+        private async Task httpClientGetAsync()
+        {
+            if (m_isUrlSelected == true)
+            {
+                HttpClient client = new HttpClient();
+                var response = await client.GetAsync(m_selectedPath);
+                var filetype = response.Content.Headers.ContentType.MediaType;
+                var imageArray = await response.Content.ReadAsByteArrayAsync();
+            }
+        }
+
+        private void urlTextBox_TextChanged(object sender, EventArgs e)
+        {
+            m_downloadUrl = urlTextBox.Text;
+
+            if (m_downloadUrl != "")
+            {
+                m_isUrlSelected = true;
+            }
+        }
+
+        private void saveTheFileDialog()
+        {
+            downloadSaveFileDialog.Title = "Save the file";
+            downloadSaveFileDialog.ShowDialog();
+
+            if (downloadSaveFileDialog.FileName != "")
+            {
+                m_isSaveSelected = true;
+            }
+            else
+            {
+                m_isSaveSelected = false;
+            }
+        }
+
+        private void invalidPath()
+        {
+            MessageBox.Show("Please select a folder", "No folder selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            saveTheFileDialog();
         }
     }
 }
